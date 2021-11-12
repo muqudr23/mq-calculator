490490a39d27521b51f551b9a9faf4d18d5507a9
Muhammad Qudratullah <87058053+muqudr23@users.noreply.github.com>
2021-11-12 18:12:08 +0700

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,193 @@
+* {
+	margin: 0px;
+	padding: 0px;
+	font-family: Viga;
+}
+
+body {
+	background: rgb(61, 153, 160);
+	height: 650px;
+}
+
+header {
+  height: 65px;
+  width: 100%;
+  background-color: rgba(34, 49, 52, 0.9);
+  position :fixed;
+  top: 0;
+  z-index: 10;
+}
+
+h2 {
+	text-align: center;
+	color: white;
+	text-transform: uppercase;
+}
+
+p {
+	text-align: center;
+	color: white;
+}
+
+.main {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translateX(-50%) translateY(-50%);
+	width: fixed;
+	background: rgba(0, 15, 15, 0.514);
+	padding: 25px;
+	border-radius: 5px;
+	box-shadow: 0 0 5px;
+}
+
+/* Bacground Animation */
+.bubbles{
+  position: absolute;
+  bottom: -100px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 70px;
+  width: 100%;
+  justify-content: space-around;
+}
+
+.bubbles span{
+  height: 60px;
+  width: 60px;
+  border-radius: 75px;
+  background: rgba(255, 255, 255, 0.1);
+  animation: move 10s linear infinite;
+  position: relative;
+  overflow: hidden;
+}
+
+@keyframes move {
+  100% {
+    transform: translateY(-100vh);
+  }
+}
+
+.bubbles span.one {
+  animation-delay: 2.2s;
+  transform: scale(2.15)
+}
+
+.bubbles span.two {
+  animation-delay: 3.5s;
+  transform: scale(1.55)
+}
+
+.bubbles span.three {
+  animation-delay: 0.2s;
+  transform: scale(0.35)
+}
+
+.bubbles span.four {
+  animation-delay: 6s;
+  transform: scale(2.15)
+}
+
+.bubbles span.five {
+  animation-delay: 7s;
+  transform: scale(0.5)
+}
+
+.bubbles span.six {
+  animation-delay: 4s;
+  transform: scale(2.5)
+}
+
+.bubbles span.seven {
+  animation-delay: 3
+  transform: scale(1.5)
+}
+
+.bubbles span:before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 60px;
+  width: 40%;
+  transform: skew(45deg) translateX(150px);
+  background: rgba(142, 223, 198, 0.363);
+  animation: 3s linear infinite;
+}
+
+.bubbles span.one:before {
+  animation-delay: 1.5s;
+}
+
+.bubbles span.two:before {
+  animation-delay: 3.5s;
+}
+
+.bubbles span.three:before {
+  animation-delay: 2.5s;
+}
+
+.bubbles span.four:before {
+  animation-delay: 7.5s;
+}
+
+.bubbles span.five:before {
+  animation-delay: 4.5s;
+}
+
+.bubbles span.six:before {
+  animation-delay: 0.5s;
+}
+
+.bubbles span.seven:before{
+  animation-delay: 6s;
+}
+
+/* Button & Textarea */
+.button {
+	width: 50px;
+	height: 50px;
+	font-size: 25px;
+	margin: 2px;
+	cursor: pointer;
+	border: none;
+	background: rgba(96, 125, 139, 0.651);
+	color: white;
+	border-radius: 10px;
+	cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.button:hover {
+  background: #49fae2;
+  color: #008a8a;
+}
+
+.btn-blue {
+	background:rgba(95, 191, 247, 0.623);
+	color:#2e4242;
+}
+
+.btn-blue:hover {
+  background: #49fae2;
+  color: #1d6666;
+}
+
+.button:active {
+  background: #b5e9e2;
+  color: #2e4242;
+  animation-delay: 2.5s;
+}
+
+.textarea {
+	width: 198px;
+  height: 45px;
+	margin: 4px;
+	font-size: 20px;
+	padding: 10px;
+	border: none;
+  background: #b8f7f3b2;
+	color: #607d8b;
+	border-radius: 5px;
+	text-align: right;
+}
