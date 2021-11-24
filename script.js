9c23ada076ded4e87911fbcac8178702de4813f4
Muhammad Qudratullah <87058053+muqudr23@users.noreply.github.com>
2021-11-24 23:54:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function insert(num) {
 	document.form.textarea.value = document.form.textarea.value + num;
 }
 
-// Calculate
+// Calculation
 function equal() {
 	var result = document.form.textarea.value;
 	document.form.textarea.value = eval(result);
 }
 
-// Clean
+// AC Button
 function clean() {
 	document.form.textarea.value = "";
 }
 
-// Delete
+// Del Button
 function back() {
 	var result = document.form.textarea.value;
 	document.form.textarea.value = result.substring(0, result.length -1);
